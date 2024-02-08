fcea171883295984f894e4c3ee9b3d9f6fbf6d92
Angelo Netho <angelonetho@gmail.com>
2024-02-08 17:41:12 -0300

    Implementando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
+import fastifyCookie from '@fastify/cookie'
 import fastify from 'fastify'
 
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions').select('*')
-
-  return transactions
+app.register(fastifyCookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
